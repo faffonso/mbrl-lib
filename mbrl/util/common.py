712245739be232ec8ca9bae5a9b4e685e06ee026
faffonso <francisco.affonso02@usp.br>
2024-05-22 16:01:51 -0500

    Forward method

--- mbrl/util/common.py
+++ mbrl/util/common.py
@@ -85,6 +85,7 @@ def create_one_dim_tr_model(
         model_cfg.out_size = obs_shape[0] + int(cfg.algorithm.learned_rewards)
     if model_cfg.get("act_size", None) is None:
         model_cfg.act_size = (act_shape[0] if act_shape else 1)   
+
     # Now instantiate the model
     print(f'Obs size {obs_shape[0]}')
     print(f'Act size {act_shape[0]}')
@@ -110,7 +111,7 @@ def create_one_dim_tr_model(
         no_delta_list=cfg.overrides.get("no_delta_list", None),
         num_elites=cfg.overrides.get("num_elites", None),
     )
-    model_dir = '/home/faffonso/Documents/mbrl-lib/exp/pets/default/pets_halfcheetah/2024.05.21/004901'
+    # model_dir = '/home/faffonso/Documents/mbrl-lib/exp/pets/default/pets_halfcheetah/2024.05.21/004901'
 
     if model_dir:
         dynamics_model.load(model_dir)
